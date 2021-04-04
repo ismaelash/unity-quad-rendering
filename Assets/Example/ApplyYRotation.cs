960771fb019d610a6d94c2eac06493353119f19b
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2021-04-04 16:25:05 -0300

    feature: quad rendering demo project for ismael

---
+++ Assets/Example/ApplyYRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class ApplyYRotation : MonoBehaviour
+{
+    [SerializeField] [Range(0f, 50f)] private float speed;
+    void Update()
+    {
+        transform.Rotate( Vector3.up * speed * Time.deltaTime, Space.World );
+    }
+}
